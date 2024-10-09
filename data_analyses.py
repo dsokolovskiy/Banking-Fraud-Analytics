a1bbcb007b39e22914cee32015a801279dcda217
dsokolovskiy <souldead147@gmail.com>
2024-10-09 10:00:34 +0300

    prepare features and split dataset into train and test sets

--- data_analyses.py
+++ data_analyses.py
@@ -1,4 +1,10 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 
 df = pd.read_csv("data/dataset.csv")
@@ -8,4 +14,9 @@ print(df.head())
 print(df.isnull().sum())
 
 df.fillna(df.mean(), inplace=True)
+
+X = df.drop("target", axis=1)
+y = df["target"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
